fef49f53aa9148133ab53824324b240247534999
robin <git@robinkirkman.com>
2012-02-13 23:50:05 -0500

    Restore unreachable bonus when painting

--- trunk/src/org/tetrevil/Fitness.java
+++ trunk/src/org/tetrevil/Fitness.java
@@ -10,7 +10,7 @@ public class Fitness {
 		for(int x = Field.BUFFER; x < Field.WIDTH + Field.BUFFER; x++) {
 			int bonus = 0;
 			for(int y = Field.HEIGHT  + Field.BUFFER - 1; y >= Field.BUFFER; y--) {
-				if(f[y][x] != null)
+				if(f[y][x] != null && f[y][x] != Block.X)
 					score += Field.HEIGHT + Field.BUFFER - y + bonus * 10;
 				else
 					bonus++;
