655eea3fb03fadc04fa54c2fe660d2af8f28e80c
robin <git@robinkirkman.com>
2012-02-13 23:03:33 -0500

    Fix painting

--- trunk/src/org/tetrevil/MaliciousRandomizer.java
+++ trunk/src/org/tetrevil/MaliciousRandomizer.java
@@ -167,11 +167,11 @@ public class MaliciousRandomizer implements Randomizer {
 		}
 		for(int y = 1; y < Field.BUFFER + Field.HEIGHT; y++) {
 			for(int x = Field.BUFFER; x < Field.BUFFER + Field.WIDTH; x++) {
-				if(f[y-1][x] == null || f[y][x-1] == null || f[y][x+1] == null && f[y][x] == Block.X)
+				if((f[y-1][x] == null || f[y][x-1] == null || f[y][x+1] == null) && f[y][x] == Block.X)
 					f[y][x] = null;
 			}
 			for(int x = Field.BUFFER + Field.WIDTH - 1; x >= Field.BUFFER; x--) {
-				if(f[y-1][x] == null || f[y][x-1] == null || f[y][x+1] == null && f[y][x] == Block.X)
+				if((f[y-1][x] == null || f[y][x-1] == null || f[y][x+1] == null) && f[y][x] == Block.X)
 					f[y][x] = null;
 			}
 		}
