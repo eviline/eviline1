61cd02c6305d80604665eb6e2bcbcbcea9731174
robin <git@robinkirkman.com>
2012-03-24 18:20:26 -0400

    Revampled standalone app to use full-screen.

--- trunk/src/org/tetrevil/runner/Main.java
+++ trunk/src/org/tetrevil/runner/Main.java
@@ -1,9 +1,20 @@
 package org.tetrevil.runner;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+import org.tetrevil.Field;
+import org.tetrevil.swing.TetrevilComponent;
+import org.tetrevil.swing.TetrevilKeyPanel;
 
 public class Main {
 
@@ -13,29 +24,39 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		final JFrame frame = new JFrame("Tetrevil");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
+		frame.setLayout(new GridBagLayout());
+		frame.setBackground(Color.BLACK);
 		
-		final MainApplet applet = new MainApplet();
+		Field field = new Field(true);
 		
-		for(String arg : args) {
-			if(arg.contains("=")) {
-				String[] f = arg.split("=", 2);
-				applet.setParameter(f[0], f[1]);
-			}
-		}
+		final TetrevilComponent tc = new TetrevilComponent(field);
 		
-		applet.init();
+		TetrevilKeyPanel tkp = new TetrevilKeyPanel(tc.getTetrevilKeyListener());
 		
-		frame.add(applet, BorderLayout.CENTER);
+//		for(String arg : args) {
+//			if(arg.contains("=")) {
+//				String[] f = arg.split("=", 2);
+//				applet.setParameter(f[0], f[1]);
+//			}
+//		}
+//		
+//		applet.init();
+		
+		GridBagConstraints c = new GridBagConstraints(0, 0, 1, 2, 0, 1, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0);
+		
+		frame.add(tc, c);
+		c.gridx++; c.weightx = 1; frame.add(new JLabel(" "), c);
+		c.gridx++; c.weightx = 0; c.gridheight = 1; c.weighty = 0; frame.add(tkp, c);
+		c.gridy++; c.weighty = 1; frame.add(new JLabel(" "), c);
+		frame.setUndecorated(true);
 		frame.pack();
-		frame.setSize(520, 500);
-		
 		frame.setVisible(true);
 		
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				applet.start();
+				frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				tc.start();
 			}
 		});
 	}
--- trunk/src/org/tetrevil/swing/TetrevilComponent.java
+++ trunk/src/org/tetrevil/swing/TetrevilComponent.java
@@ -1,9 +1,14 @@
 package org.tetrevil.swing;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 import org.tetrevil.Field;
 
@@ -19,6 +24,19 @@ public class TetrevilComponent extends JScrollPane {
 	private static final long serialVersionUID = 1L;
 	protected Field field;
 	protected TetrevilTable table;
+	protected TetrevilKeyListener tetrevilKeyListener;
+	
+	protected Timer ticker = new Timer(1000, new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			field.clockTick();
+		}
+	});
+	{{
+		ticker.setRepeats(true);
+		ticker.setInitialDelay(500);
+		ticker.setDelay(1000);
+	}}
 	
 	public TetrevilComponent(Field field) {
 		super(new TetrevilTable(field), JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -31,6 +49,42 @@ public class TetrevilComponent extends JScrollPane {
 				scroll.getViewport().getView().setSize(scroll.getViewport().getExtentSize());
 			}
 		});
+		
+		tetrevilKeyListener = new TetrevilKeyListener(field);
+		getTable().addKeyListener(tetrevilKeyListener);
+		addKeyListener(tetrevilKeyListener);
+	}
+	
+	@Override
+	public void setSize(Dimension d) {
+		setSize(d.width, d.height);
+	}
+	
+	@Override
+	public void setSize(int width, int height) {
+		if(width * 2 > height)
+			width = height / 2;
+		else if(height / 2 > width)
+			height = width * 2;
+		super.setSize(width, height);
+	}
+	
+	public void start() {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if(!getTable().isFocusOwner()) {
+					getTable().requestFocusInWindow();
+					SwingUtilities.invokeLater(this);
+				} else {
+					ticker.start();
+				}
+			}
+		});
+	}
+	
+	public void stop() {
+		ticker.stop();
 	}
 
 	public Field getField() {
@@ -45,4 +99,9 @@ public class TetrevilComponent extends JScrollPane {
 	public TetrevilTable getTable() {
 		return table;
 	}
+	
+	public TetrevilKeyListener getTetrevilKeyListener() {
+		return tetrevilKeyListener;
+	}
 }
+
