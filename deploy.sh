a62385891556c6f0180fce33414f217c8dc0501c
robin <git@robinkirkman.com>
2012-04-12 19:57:58 -0400

    Adjust deploy script after moving out of trunk/

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ant
 ./sign_jar.sh
-scp dist/* ../../tetrevil_www/WebContent/* robin@www.tetrevil.org:/var/www/tetrevil
+scp dist/* ../tetrevil_www/WebContent/* robin@www.tetrevil.org:/var/www/tetrevil
 
