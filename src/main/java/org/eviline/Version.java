ea8c6af537d46c5b92d0e944010c956ab27f8280
rkirkman <rkirkman@mitre.org>
2012-10-15 09:56:00 -0400

    First cut at pulling the version from the maven resource

--- src/main/java/org/eviline/Version.java
+++ src/main/java/org/eviline/Version.java
@@ -1,7 +1,26 @@
 package org.eviline;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
 public class Version {
 	public static String getVersion() {
-		return "1.0.4";
+		URL url = Version.class.getClassLoader().getResource("META-INF/maven/org.eviline/eviline/pom.properties");
+		if(url == null)
+			return "0.0.0-DEVEL";
+		try {
+			Properties p = new Properties();
+			InputStream in = url.openStream();
+			try {
+				p.load(in);
+			} finally {
+				in.close();
+			}
+			return p.getProperty("version");
+		} catch(IOException ioe) {
+			return "0.0.0-ERROR";
+		}
 	}
 }
