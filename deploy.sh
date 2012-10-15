7fcf21ed93f1d6354fba225509898914630c03f4
robin <robin@robinkirkman.com>
2012-10-15 01:22:21 -0400

    Working deploy system.  Woot.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ant
+mvn package
 ./sign_jar.sh
-cp dist/* ../eviline_www/WebContent
+cp target/*.jar *.jnlp ../eviline_www/WebContent
 cd ../eviline_www
 git commit -a -m "Deploying"
 git push
--- sign_jar.sh
+++ sign_jar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jarsigner -keystore keystore -storepass `cat keystore_password` -signedjar dist/eviline_signed.jar dist/eviline.jar signFiles
+jarsigner -keystore keystore -storepass `cat keystore_password` -signedjar target/eviline_signed.jar target/eviline.jar signFiles
