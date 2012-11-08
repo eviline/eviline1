1f1ed30a888d5d752631323088c22f8e2efb0770
robin <robin@robinkirkman.com>
2012-11-08 17:56:55 -0500

    A main class?

---
+++ eviline-console/src/main/java/org/eviline/console/Main.java
@@ -0,0 +1,15 @@
+package org.eviline.console;
+
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.gui.GUIScreen;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GUIScreen gui = TerminalFacade.createGUIScreen();
+	}
+
+}
