31a661a15e66eee717a89e34d38fbf3c934405b1
robin <robin@robinkirkman.com>
2012-11-10 15:40:12 -0500

    Documentation

--- eviline-core/src/main/java/org/eviline/BasicPropertySource.java
+++ eviline-core/src/main/java/org/eviline/BasicPropertySource.java
@@ -6,19 +6,38 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.TreeMap;
 
+/**
+ * A basic implementation of {@link PropertySource} backed by a {@link Map}.
+ * @author robin
+ *
+ */
 public class BasicPropertySource implements PropertySource, Serializable {
 	private static final long serialVersionUID = 0;
 	
+	/**
+	 * The map that backs this {@link BasicPropertySource}.
+	 */
 	protected Map<String, String> map;
 	
+	/**
+	 * Create a new {@link BasicPropertySource} backed by a {@link TreeMap}
+	 */
 	public BasicPropertySource() {
 		this(new TreeMap<String, String>());
 	}
 	
+	/**
+	 * Create a {@link BasicPropertySource} backed by the argument {@link Properties}.
+	 * @param p
+	 */
 	public BasicPropertySource(Properties p) {
 		this((Map) p);
 	}
 	
+	/**
+	 * Create a {@link BasicPropertySource} backed by the argument {@link Map}.
+	 * @param p
+	 */
 	public BasicPropertySource(Map<String, String> p) {
 		this.map = p;
 	}
