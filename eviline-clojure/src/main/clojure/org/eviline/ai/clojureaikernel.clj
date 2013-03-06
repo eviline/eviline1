bae6a2a7569ebc5bb49269cc5438fd0ddb5ac503
Robin Kirkman <robin@robinkirkman.com>
2013-03-06 01:17:04 -0500

    Getting there with clojure

--- eviline-clojure/src/main/clojure/org/eviline/ai/clojureaikernel.clj
+++ eviline-clojure/src/main/clojure/org/eviline/ai/clojureaikernel.clj
@@ -1,3 +1,13 @@
-(ns org.eviline.ai.clojureaikernel)
+(ns org.eviline.ai.clojureaikernel
+  (:gen-class 
+    :name org.eviline.ai.ClojureAIKernel
+    :methods [#^{:static true} [ newAIKernel [] org.eviline.ai.AIKernel]])
+  )
 (clojure.core/use 'clojure.core)
-(import '(org.eviline.ai AIKernel))
+(import '(org.eviline.ai AIKernel Context Decision DecisionModifier QueueContext))
+
+(defn -newAIKernel []
+  (proxy [AIKernel] []
+    
+    )
+  )
