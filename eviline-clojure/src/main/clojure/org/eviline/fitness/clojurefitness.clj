fb28e309a4801df65c220a987fa865a0bb69174e
Robin Kirkman <robin@robinkirkman.com>
2013-03-06 04:33:46 -0500

    More work on the fitness function.

--- eviline-clojure/src/main/clojure/org/eviline/fitness/clojurefitness.clj
+++ eviline-clojure/src/main/clojure/org/eviline/fitness/clojurefitness.clj
@@ -14,16 +14,30 @@
   (reduce + (map-indexed count-row (reverse (.getField field))))
   )
 
+(defn paint-impossibles-row [memo row]
+  (let [
+        prev (if (= 0 (count memo)) (repeat (+ Field/WIDTH Field/BUFFER Field/BUFFER) nil) (peek memo))
+        m-row (map (fn [b] (if (nil? b) Block/M b)) row)
+        m-nils-row (map-indexed (fn [i b] (if (nil? (nth prev i)) nil b)) m-row)
+        paint-across (fn [r b] (conj r (if (and (nil? (peek r)) (= Block/M b)) nil b)))
+        m-right-row (reduce paint-across [] m-nils-row)
+        m-left-row (reduce paint-across [] (reverse m-right-row))
+        painted-row (reverse m-left-row)
+        ]
+    (conj memo (vec painted-row))
+    )
+  )
+
 (defn paint-impossibles [field]
-  field
+  (reduce paint-impossibles-row [] (vec field))
   )
 
 (defn count-impossibles-row [rev-index row]
-  
+  (* rev-index (count (filter #(= Block/M %) row)))
   )
 
 (defn count-impossibles [field] 
-  (let (painted (paint-impossibles (.getField field)))
+  (let [painted (paint-impossibles (.getField field))]
     (reduce + (map-indexed count-impossibles-row (reverse painted)))
     )
   )
@@ -31,6 +45,7 @@
 (defn score [field] 
   (+
     (count-field field)
+    (count-impossibles field)
     ))
 
 (defn -newFitness []
