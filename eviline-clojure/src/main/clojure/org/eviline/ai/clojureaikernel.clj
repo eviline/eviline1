f87f624518ee1355ece27c7b8c096ebedae319db
Robin Kirkman <robin@robinkirkman.com>
2013-03-06 01:50:45 -0500

    Tweaking stuff.

--- eviline-clojure/src/main/clojure/org/eviline/ai/clojureaikernel.clj
+++ eviline-clojure/src/main/clojure/org/eviline/ai/clojureaikernel.clj
@@ -1,13 +1,13 @@
 (ns org.eviline.ai.clojureaikernel
   (:gen-class 
     :name org.eviline.ai.ClojureAIKernel
-    :methods [#^{:static true} [ newAIKernel [] org.eviline.ai.AIKernel]])
+    :methods [#^{:static true} [ newAIKernel [org.eviline.fitness.Fitness] org.eviline.ai.AIKernel]])
   )
 (clojure.core/use 'clojure.core)
 (import '(org.eviline.ai AIKernel Context Decision DecisionModifier QueueContext))
 
-(defn -newAIKernel []
+(defn -newAIKernel [fitness]
   (proxy [AIKernel] []
-    
+    (getFitness [] fitness)
     )
   )
