8e8ec318c342e8fc7ff6c6e64697b0a35c3ce6f2
Robin Kirkman <robin@robinkirkman.com>
2013-03-06 05:10:22 -0500

    Stub classes and update pom/.project to pacify Eclipse using clojure-generated classes in other projects

---
+++ eviline-clojure/src/main/clojure/org/eviline/clj/ClojureAIKernel.java
@@ -0,0 +1,9 @@
+package org.eviline.clj;
+
+import org.eviline.ai.AIKernel;
+
+public class ClojureAIKernel {
+	public static AIKernel newAIKernel() {
+		throw new InternalError("This class should be in clojure");
+	}
+}
---
+++ eviline-clojure/src/main/clojure/org/eviline/clj/ClojureFitness.java
@@ -0,0 +1,9 @@
+package org.eviline.clj;
+
+import org.eviline.fitness.Fitness;
+
+public class ClojureFitness {
+	public static Fitness newFitness() {
+		throw new InternalError("This class should be in clojure");
+	}
+}
