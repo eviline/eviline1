450b4deae35c87c4be7f07dded9f77b3504d438b
Robin Kirkman <robin@robinkirkman.com>
2013-03-06 19:28:12 -0500

    Use a record instead of a vector for the shape-x-y triplet

--- eviline-clojure/src/main/clojure/org/eviline/clj/clojureaikernel.clj
+++ eviline-clojure/src/main/clojure/org/eviline/clj/clojureaikernel.clj
@@ -16,28 +16,28 @@
 (defn -post-init [this fitness] (.superSetFitness this fitness))
 (defn -setFitness [this fitness] (throw (UnsupportedOperationException.)))
 
-
+(defrecord ShapeXY [shape x y])
 (def x-range (range (- Field/BUFFER 2) (+ Field/BUFFER Field/WIDTH 2)))
 (def y-range (range 0 (+ Field/BUFFER Field/HEIGHT 2)))
 (defn shape-intersects? [^Field field ^Shape shape ^long x ^long y]
-  (if (.intersects shape (.getField field) x y) [x y]))
+  (if (.intersects shape (.getField field) x y) (->ShapeXY shape x y)))
 (defn y-intersections [^Field field ^Shape shape ^long x]
   (remove nil? (map shape-intersects? (repeat field) (repeat shape) (repeat x) y-range)))
 (defn y-grounded-reducer [grounded xy]
-  (cons xy (if (= (inc (second (first grounded))) (second xy)) (rest grounded) (cons xy (rest grounded))))
+  (cons xy (if (= (inc (:y (first grounded))) (:y xy)) (rest grounded) (cons xy (rest grounded))))
   )
 (defn y-grounded 
   ([xy-pairs]
-    (rest (reduce y-grounded-reducer (repeat (if (= 0 (second (first xy-pairs))) 1 2) (first xy-pairs)) (rest xy-pairs)))
+    (rest (reduce y-grounded-reducer (repeat (if (= 0 (:y (first xy-pairs))) 1 2) (first xy-pairs)) (rest xy-pairs)))
     )
   ([^Field field ^Shape shape ^long x]
-    (map #(cons shape %) (y-grounded (y-intersections field shape x))))
+    (y-grounded (y-intersections field shape x)))
   ([^Field field ^Shape shape]
-    (mapcat (fn [x] (y-grounded field shape x)) x-range))
+    (mapcat y-grounded (repeat field) (repeat shape) x-range))
   )
 (defn grounded-locations 
   ([^Field field ^ShapeType type] 
-    (mapcat (fn [shape] (y-grounded field shape)) (.searchOrientations type))
+    (mapcat y-grounded (repeat field) (.searchOrientations type))
     )
   )
 
