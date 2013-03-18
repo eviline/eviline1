107b1c9b7584bab21f363cdc35c2095bc16b4bb8
Robin Kirkman <robin@robinkirkman.com>
2013-03-17 21:48:01 -0400

    Messing with the site.

--- eviline-tomcat/src/main/webapp/head.js
+++ eviline-tomcat/src/main/webapp/head.js
@@ -20,7 +20,7 @@ function customize() {
 }           
 
 function play() {
-    var attributes = { code:'org.eviline.runner.MainApplet',  width:550, height:550} ;
+    var attributes = { code:'org.eviline.runner.MainApplet',  width:'100%', height:'100%'} ;
     var parameters = {jnlp_href: 'eviline.jnlp', score_host:'www.eviline.org:8080'} ;
     
     if(document.URL.toLowerCase().indexOf("www.angeviline.org") != -1) {
