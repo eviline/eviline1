20e353e38dd25f4af2a6e879167ee4fce246d77a
Robin Kirkman <robin@robinkirkman.com>
2013-04-24 23:14:27 -0400

    Don't cache the applet JNLP

--- eviline-tomcat/src/main/webapp/app/assets/javascripts/application.js
+++ eviline-tomcat/src/main/webapp/app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ function customize() {
 
 function play() {
     var attributes = { code:'org.eviline.runner.MainApplet',  width:'750', height:'750'} ;
-    var parameters = {jnlp_href: 'http://www.eviline.org:8080/eviline-tomcat/eviline.jnlp', score_host:'www.eviline.org:8080'} ;
+    var parameters = {jnlp_href: 'http://www.eviline.org:8080/eviline-tomcat/eviline.jnlp?', score_host:'www.eviline.org:8080'} ;
     
     deployJava.runApplet(attributes, parameters, '1.6');
 }
